b21bba77aa6c974c5e39e091c5fe06bb1f9b42dd
NishaNarayanaswamy <nisha.narayanaswamy@sikkasoftware.com>
2017-02-24 14:44:08 -0800

    Update app.py

--- app.py
+++ app.py
@@ -184,12 +184,12 @@ def makeWebhookResult(req):
 		elif req.get("result").get("action") == 'inventory':
 			speech = "Based on your scheduled appointments, you will require some additional supplies, totaling $100. ...Would you like to hear the list of supplies required?"
 			
-			with open('inventory.txt', 'r') as outfile:
+			with open('/app/inventory.txt', 'wb+') as outfile:
 				line = outfile.readline()
 				print " ++++++++++ read file line 1: +++++++++++++++++", line
 				
-				#outfile.write('hello world')
-				#print '+++++++++++++++++++++++++ writing to file +++++++++++++++++++++++++ '
+				outfile.write('hello world')
+				print '++++++++++++ writing to file +++++++++++++ '
 				print os.path.realpath(outfile.name)
 							
 		elif req.get("result").get("action") == 'no_inventory':
